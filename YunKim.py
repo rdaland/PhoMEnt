245bdb79fb7c4625b4ef84904cf816f72d990a41
YunKim <yun.ucla@gmail.com>
2014-01-22 15:22:45 -0800

    made some changes

--- YunKim.py
+++ YunKim.py
@@ -1,3 +1,2 @@
 print """
-The terminal code didn't work, so I just used github that is on my desk and manually changed everything.
-Hopefully I can use terminal next time to do this."""
+Now i'm using the terminal. let's hope this works!"""
