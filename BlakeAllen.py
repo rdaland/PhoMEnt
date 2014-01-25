e7c1258df946878f5a7d6a257e32e34c447851f6
bowersd <bowersd@ucla.edu>
2014-01-25 08:58:53 -0800

    We are using Python 2 right? Also, continued the joke.

--- BlakeAllen.py
+++ BlakeAllen.py
@@ -6,3 +6,7 @@ Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 ImportError: cannot import name all_the_answers""")
 print("\nI guess computational phonology isn't so easy after all...")
+
+print("Ah, the import statement was wrong is all. It needs to be 'import phonology'")
+
+#This raises an important point though. I believe we will be using Python 2, not Python 3.
