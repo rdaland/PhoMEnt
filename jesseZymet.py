2fae7750f8f67514815976d7a0ba86022b17fda9
jzymet <jzymet@gmail.com>
2014-01-29 18:19:47 -0800

    RR3
    RR3

--- jesseZymet.py
+++ jesseZymet.py
@@ -3,16 +3,22 @@ def RussianRoulette():
     print "Hi! Let's play some Russian roulette. If you survive five rounds, you win and you get to hear a secret about the author of this program. Else, you lose."
     marker = "Y"
     while marker == "Y":
-        selection = input("Pick an integer between 1 and 6. ")
-        if selection < 1 or selection > 6:
+        while True:
+            try:
+                choice = input("Pick an integer between 1 and 6. ")
+                break
+            except NameError:
+                print "You fail to adhere to instructions. You lose!"
+        if choice < 1 or choice > 6:
             print "You fail to adhere to instructions. You lose!"
         else:
             from random import randint
             count = 1
             bullet = randint(1,6)
-            while selection != bullet and count <= 5:
+            while choice != bullet and count <= 5:
                 print "You survived round", count, "!"
                 bullet = bullet%6 + 1
+                print choice, bullet
                 count = count + 1
             if count <= 5:
                 print "Sorry, you lose. Thanks for playing!"
