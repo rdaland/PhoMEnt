251f9bef5e285d6077c2ca5d179817ac08c5e47f
paigefox <paige.vict.fox@gmail.com>
2014-02-03 11:30:32 -0800

    Delete MichaelLefkowitz.py

--- MichaelLefkowitz.py
+++
@@ -1,9 +0,0 @@
-# MichaelLefkowitz.py plays a simple number game with users. 
-# Specifically, it asks them to input numbers, becomes increasingly 
-# angry at their choices, insults them, and then terminates.
-
-print ("Hello, user! Let's play a game.")
-for x in range(0, 3):
-    num = input("\nPlease enter a number: ")
-    print ("I don't like the number ", num, ("!" * (x+1)), sep='')
-print ("\nYou suck at this game. Goodbye.")
