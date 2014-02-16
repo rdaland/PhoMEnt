0dcd597295e09ebad7bd21c1897b1fbf834f19f3
michaellefkowitz <lmlefkowitz@gmail.com>
2014-02-16 01:09:29 -0800

    Updated gen.py

--- gen.py
+++ gen.py
@@ -1,50 +1,40 @@
 '''
-Several functions for generating subets of sigma star (in list form).
+Several functions for generating subets of sigma star (in list form). Namely:
+sigma_k, sigma_0k, sigma_1k, each of which takes an alphabet and a value for k.
 '''
 
-import copy
-
-# python's "append" updates lists in place rather than returning a new list,
-# which means code like this doesn't work in Python:
-
-# for word in sublanguage:
-#     for letter in alphabet:
-#         new_word = word.append(letter)
-#         language.append(new_word)
-
-# Anyone know of a better alternative than using copy.copy? 
-
 def sigma_k(alphabet, k):
-    '''returns the words in sigma k'''
+    '''returns words in sigma-k'''
     if k == 0:
-        return [[]]
+        return ['']
     else:
         language = []
         sublang = sigma_k(alphabet, k - 1)
         for word in sublang:
-            for letter in alphabet:
-                new_word = copy.copy(word)
-                new_word.append(letter)
-                language.append(new_word)
+            language += [word + a for a in alphabet]
         return language
+
+# These two functions call sigma_k lots of times, and as a result,
+# lots of values are calcuated redundantly. The efficiency could be
+# improved from O(|sigma|*k^3) to O(|sigma|*k^2), and probably should be
+# when we move to alphabets bigger than {C,V} or words longer than 6.
 
 def sigma_0k(alphabet, k):
-    '''returns the words in sigma n for all n from 0 to k'''
-    if k == 0:
-        return [[]]
-    else:
-        language = []
-        sublang = sigma_0k(alphabet, k - 1)
-        for word in sublang:
-            language.append(word)
-            for letter in alphabet:
-                new_word = copy.copy(word)
-                new_word.append(letter)
-                language.append(new_word)
-        return language
+    '''returns words in all sigma-n from n = 0 to k'''
+    language = []
+    for i in range(0,k+1):
+        language += sigma_k(alphabet, i)
+    return language
 
 def sigma_1k(alphabet, k):
-    '''returns the words in sigma n for all n from 1 to k'''
-    language = sigma_0k(alphabet, k)
-    language.remove([])
+    '''returns words in all sigma-n from n = 1 to k'''
+    language = []
+    for i in range(1,k+1):
+        language += sigma_k(alphabet, i)
     return language
+
+# Example alphabet
+cv = ['C','V']
+
+# Example language
+cv6 = sigma_1k(cv,6)
