884c0ce5e13ec8430be256b8fe9c98d477223180
bhallen <allenblakeh@gmail.com>
2014-02-16 17:50:02 -0800

    Add itertools-based functions to gen.py and provide a little data comparing it to the from-scratch functions

--- gen.py
+++ gen.py
@@ -3,6 +3,8 @@ Several functions for generating subets of sigma star (in list form). Namely:
 sigma_k, sigma_0k, sigma_1k, each of which takes an alphabet and a value for k.
 '''
 
+import itertools
+
 def sigma_k(alphabet, k):
     '''returns words in sigma-k'''
     if k == 0:
@@ -33,8 +35,52 @@ def sigma_1k(alphabet, k):
         language += sigma_k(alphabet, i)
     return language
 
+
+# These two functions do the same job as sigma_k and sigma_1k, respectively,
+# but make use of a function from itertools to do so more efficiently.
+
+def it_sigma_k(alphabet, k):
+    """Return words in sigma-k. May be necessary to 
+    have this function return a generator rather than a list eventually if 
+    memory usage becomes a problem.
+    """
+    return [''.join(tuple) for tuple in itertools.product(['C', 'V'], repeat=k)]
+
+def it_sigma_1k(alphabet, k):
+    """Return all words in all sigma-n from n=1 to k.
+    """
+    language = []
+    for i in range(1, k+1):
+        language += it_sigma_k(alphabet,i)
+    return language
+
+
 # Example alphabet
 cv = ['C','V']
 
 # Example language
 cv6 = sigma_1k(cv,6)
+
+
+"""
+Text below shows some efficiency comparisons of the sigma functions versus it_sigma functions.
+Take-away: it_sigma outperforms sigma significantly when the alphabet becomes larger,
+but the advantage of it_sigma over sigma lessens as k increases.
+
+>>> timeit("sigma_1k(['C','V'],6)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+0.06314206123352051
+>>> timeit("it_sigma_1k(['C','V'],6)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+0.037229061126708984
+>>> timeit("sigma_1k(['C','V'],8)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+0.17891502380371094
+>>> timeit("it_sigma_1k(['C','V'],8)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+0.14221501350402832
+>>> timeit("sigma_1k(['C','V'],12)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+2.4878041744232178
+>>> timeit("it_sigma_1k(['C','V'],12)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+2.331766128540039
+>>> timeit("sigma_1k(['C','V','X','Y','Z','A','B'],6)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+21.212828874588013
+>>> timeit("it_sigma_1k(['C','V','X','Y','Z','A','B'],6)", number=1000, setup="from __main__ import sigma_1k,it_sigma_1k")
+0.05199599266052246
+"""
