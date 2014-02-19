a25813b054785f839ceb86aa86846018df445cc9
michaellefkowitz <lmlefkowitz@gmail.com>
2014-02-19 14:41:20 -0800

    Replaced gen with gen2
    As part of an effort to minimize reliance on external packages, I've
    written functions for gen which are as fast (sometimes even a bit
    faster) than the imported itertools versions we were using. We no longer
    need to import itertools.py.

--- gen.py
+++ gen.py
@@ -1,35 +1,57 @@
+''' Several functions for generating subets of sigma star (in list form). Namely:
+sigma_k, sigma_0k, sigma_1k each of which takes an alphabet and a value for k, and returns a list.
 '''
-Several functions for generating subets of sigma star (in list form). Namely:
-sigma_k, sigma_0k, sigma_1k, each of which takes an alphabet and a value for k.
-'''
-
-import itertools
 
 def sigma_k(alphabet, k):
-    """Return words in sigma-k. May be necessary to 
-    have this function return a generator rather than a list eventually if 
-    memory usage becomes a problem.
-    """
-    return [' '.join(word) for word in itertools.product(alphabet, repeat=k)]
+    '''returns words in sigma-k'''
+    def helper(i):
+        if i == 0:
+            ilang = ['']
+        else:
+            ilang = []
+            hlang = sigma_k(alphabet, k - 1)
+            for word in hlang:
+                ilang += [word + a for a in alphabet]
+        return ilang
+    return helper(k)
 
 def sigma_0k(alphabet, k):
-    """Return all words in all sigma-n from n=1 to k.
-    """
-    language = []
-    for i in range(0, k+1):
-        language += sigma_k(alphabet,i)
-    return language
+    '''Return all words in all sigma-n from n=1 to k.'''
+    assert k >= 0
+    def helper(i):
+        language = []
+        if i == 0:
+            ilang = ['']
+            language = ['']
+        else:
+            ilang = []
+            hlang, language = helper(i - 1)
+            for word in hlang:
+                ilang += [word + a for a in alphabet]
+                language += [word + a for a in alphabet]
+        return (ilang, language)
+    return helper(k)[1]
 
 def sigma_1k(alphabet, k):
-    """Return all words in all sigma-n from n=1 to k.
-    """
-    language = []
-    for i in range(1, k+1):
-        language += sigma_k(alphabet,i)
-    return language
+    '''Return all words in all sigma-n from n=1 to k.'''
+    assert k > 0
+    def helper(i):
+        language = []
+        if i == 0:
+            ilang = ['']
+            language = [] #Only difference for sigma 1k
+        else:
+            ilang = []
+            hlang, language = helper(i - 1)
+            for word in hlang:
+                ilang += [word + a for a in alphabet]
+                language += [word + a for a in alphabet]
+        return (ilang, language)
+    return helper(k)[1]
 
-# Example alphabet
+# Example alphabets
 cv = ['C','V']
+alpha = ['a','b','c','d','e','f','g']
 
 # Example language
-cv6 = sigma_1k(cv,6)
+cv6 = sigma_0k(cv,6)
