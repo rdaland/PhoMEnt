b44d1d70a8bf8ed82f22243f5530067d73a2c1ff
bowersd <bowersd@ucla.edu>
2014-02-19 21:35:00 -0800

    Adding minor fixes from class

--- AlternateInput.py
+++ AlternateInput.py
@@ -1,8 +1,8 @@
 import megatableau
 import data_prob
 import gen
-import calc_weights
-import regex as re
+##import calc_weights
+import re
 
 def readDataOnly(dataFile):
     tableau = megatableau.MegaTableau()
@@ -13,44 +13,44 @@ def readDataOnly(dataFile):
                 print "lines need to at least have an ouput and a frequency"
                 return
             elif len(parsed) == 2: #only outputs in file
-                tableau.tableau["NEW-WORD"][parsed[0]] = [parsed[1], None, 0]
+                tableau.tableau["NEW-WORD"][parsed[0]] = [float(parsed[1]), None, 0]
             elif len(parsed) == 3: #inputs, outputs, freq
-                tableau.tableau[parsed[0]][parsed[1]] = [parsed[2], None, 0]
+                tableau.tableau[parsed[0]][parsed[1]] = [float(parsed[2]), None, 0]
     return tableau
 
 def violations(constraint,word):
-    return len(constraint.findall(word, overlapped = True))
+    return len(constraint.findall(word))#, overlapped = True))
 
-def applyMarkList(tableau, markList):
+def applyMarkList(mt, markList):
     """
     Apply markedness constraints to each SR in tableau
     """
     #only add constraint violations if you haven't already added these constraints to the tableau
-    if all([con not in tableau.constraints for con in markList]):
-        for UR in tableau:
-            for SR in tableau[UR]:
-                if not tableau[UR][SR][1]:
-                    tableau[UR][SR][1] = [violations(re.compile(con),SR) for con in markList]
+    if all([con not in mt.constraints for con in markList]):
+        for UR in mt.tableau:
+            for SR in mt.tableau[UR]:
+                if not mt.tableau[UR][SR][1]:
+                    mt.tableau[UR][SR][1] = [violations(re.compile(con),SR) for con in markList]
                 else:
-                    tableau[UR][SR][1].extend([violations(re.compile(con),SR) for con in markList])
-        tableau.constraints.extend(markList)
-        tableau.constraints_abbrev.extend(markList)
-    return tableau
+                    mt.tableau[UR][SR][1].extend([violations(re.compile(con),SR) for con in markList])
+        mt.constraints.extend(markList)
+        mt.constraints_abbrev.extend(markList)
+    return mt
     
-def applyFaithList(tableau, faithList):
+def applyFaithList(mt, faithList):
     """
     Apply faith constraints to each SR in tableau
     """
     #only add constraint violations if you haven't already added these constraints to the tableau
-    if all([con not in tableau.constraints for con in faithList]):
-        for UR in tableau:
-            for SR in tableau[UR]:
-                if not tableau[UR][SR][1]:
-                    tableau[UR][SR][1] = [con((UR, SR)) for con in faithList]
+    if all([con not in mt.constraints for con in faithList]):
+        for UR in mt.tableau:
+            for SR in mt.tableau[UR]:
+                if not mt.tableau[UR][SR][1]:
+                    mt.tableau[UR][SR][1] = [con((UR, SR)) for con in faithList]
                 else:
-                    tableau[UR][SR][1].extend([con((UR, SR)) for con in faithList])
-        tableau.constraints.extend(markList)
-        tableau.constrants_abbrev.extend(markList)
-    return tableau
+                    mt.tableau[UR][SR][1].extend([con((UR, SR)) for con in faithList])
+        mt.constraints.extend(markList)
+        mt.constrants_abbrev.extend(markList)
+    return mt
 
 samplemarklist = ['[cv]','^v','c$','cc','vv']
