c1a22a6c79b29ec9583b16f31c3ba70a6a6f16c4
bowersd <bowersd@ucla.edu>
2014-02-24 12:10:45 -0800

    see if it works with itworks.py

---
+++ itworks.py
@@ -0,0 +1,14 @@
+
+import AlternateInput as ai
+import calc_weights
+
+mt = ai.readDataOnly('phx_train.txt')
+
+ai.augment_sigmak(mt, ['c', 'v'], 6)
+
+newCons = ['[cv]','^v','c$','c c','v v', '^c c', 'c c c$', 'c c c c']
+
+ai.applyMarkList(mt, newCons)
+
+calc_weights.learn_weights(mt)
+
