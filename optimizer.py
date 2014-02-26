25cbbd528fa93a6b9025094671de60c072e890c6
michaellefkowitz <lmlefkowitz@gmail.com>
2014-02-26 15:58:02 -0800

    optimizer.py now works and is pretty

--- optimizer.py
+++ optimizer.py
@@ -1,25 +1,9 @@
-import sys
-import megatableau, data_prob
+''' A moduled used by maxent.py and phlearn.py to find the ideal weights for a tableau.
+'''
+
+import megatableau
 import scipy, scipy.optimize
 import math
-
-def learn_weights(mt):
-    """ Given a filled-in megatableau, optimize and return its weight vector.
-    """
-    # Set up the initial weights and weight bounds (nonpositive reals)
-    w_0 = -scipy.rand(len(mt.weights))
-    nonpos_reals = [(-25,0) for wt in mt.weights]
-
-    # Learn
-    print(data_prob.nlpwg(w_0, mt.tableau))
-    learned_weights, fneval, rc = scipy.optimize.fmin_tnc(data_prob.nlpwg, w_0, args = (mt.tableau,), bounds=nonpos_reals)
-
-    ## Previous version below: tells fmin_tnc to approximate the gradient itself
-    # learned_weights, fneval, rc = scipy.optimize.fmin_tnc(data_prob.neg_log_probability, w_0, args = (mt.tableau,), bounds=nonpos_reals, approx_grad=True)
-
-    return learned_weights
-
-
 
 ### HELPER FUNCTIONS FOR CALCULATING PROBABILITY ###
 
@@ -48,10 +32,11 @@ def update_maxent_values(weights, tableau):
             tableau[ur][sr][2] = maxent_value(weights, tableau, ur, sr)
 
 
-### OBJECTIVE FUNCTIONS ###
+### OBJECTIVE FUNCTION(S) ###
 
 def neg_log_probability_with_gradient(weights, tableau):
     """ Returns the negative log probability of the data AND a gradient vector.
+    This is the objective function used in learn_weights().
     """
     update_maxent_values(weights, tableau)
     logProbDat = 0
@@ -68,28 +53,46 @@ def neg_log_probability_with_gradient(weights, tableau):
                 observed[c] += tableau[ur][sr][1][c] * tableau[ur][sr][0]
                 expected[c] += tableau[ur][sr][1][c] * prob
     expected[:] = [x * data_size for x in expected] # multiply expected values by size of data
-    #print observed
-    #print expected
-    gradient = [y - x for x, y in zip(observed, expected)] # i.e. observed minus expected
+    gradient = [e - o for o, e in zip(observed, expected)] # i.e. -(observed minus expected)
     return (-logProbDat, gradient)
 
 nlpwg = neg_log_probability_with_gradient # So you don't get carpal tunnel syndrome.
 
 def neg_log_probability(weights, tableau):
-    """ Returns the negative log probability of the data.
+    """ Returns just the negative log probability of the data.
+    This function isn't currently used, it's just here in case you want it.
     """
-    update_maxent_values(weights, tableau)
-    logProbDat = 0
-    for ur in tableau:
-        z = z_score(tableau, ur)
-        for sr in tableau[ur]:
-            prob = tableau[ur][sr][2] / z
-            logProbDat += math.log(prob) * tableau[ur][sr][0] 
-    return - logProbDat
+    return (nlpwg(weights, tableau))[0]
 
 def probability(weights, tableau):
-    """ Returns the probability of the data.
-    This function just makes a call to neg_log_probability, and
-    transforms the results out of the log space.
+    """ Returns just the probability of the data.
+    This function isn't currently used, it's just here in case you want it.
     """
-    return math.exp(- neg_log_probability(weights, tableau))
+    return math.exp(-(nlpwg(weights, tableau))[0])
+
+
+### OPTIMIZATION FUNCTION
+
+def learn_weights(mt):
+    """ Given a filled-in megatableau, return the optimal weight vector.
+    """
+    # Set up the initial weights and weight bounds (nonpositive reals)
+    w_0 = -scipy.rand(len(mt.weights))
+    nonpos_reals = [(-25,0) for wt in mt.weights]
+
+    # Find the best weights
+    learned_weights, fneval, rc = scipy.optimize.fmin_tnc(nlpwg, w_0, args = (mt.tableau,), bounds=nonpos_reals)
+
+    # Update the mt in place with the new weights
+    mt.weights = learned_weights
+
+    # Be sociable
+    print "Initial (neglogprob, gradient):", (nlpwg(w_0, mt.tableau))
+    print "Final   (neglogprob, gradient):", (nlpwg(learned_weights, mt.tableau))
+    print "\nBoom! Weights have been updated:"
+    for i in range(0,len(learned_weights)):
+        print mt.constraints_abbrev[i],"\t",learned_weights[i]
+    print ""
+
+    # Return
+    return learned_weights
