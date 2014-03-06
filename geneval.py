800358371412487a70ec130f02b0c3d16e3c862b
michaellefkowitz <lmlefkowitz@gmail.com>
2014-03-05 17:09:01 -0800

    updated geneval to treat violations as lists.

--- geneval.py
+++ geneval.py
@@ -13,9 +13,9 @@ def read_data_only(mt, dataFile):
                 print "lines need to at least have an ouput and a frequency"
                 return
             elif len(parsed) == 2: #only outputs in file
-                mt.tableau["NEW-WORD"][parsed[0]] = [float(parsed[1]), None, 0]
+                mt.tableau["NEW-WORD"][parsed[0]] = [float(parsed[1]), {}, 0]
             elif len(parsed) == 3: #inputs, outputs, freq
-                mt.tableau[parsed[0]][parsed[1]] = [float(parsed[2]), None, 0]
+                mt.tableau[parsed[0]][parsed[1]] = [float(parsed[2]), {}, 0]
 
 ## read in constraints and turn them into a list of strings called `constraints`
 def read_constraints(mt, constraintFile):
@@ -71,7 +71,7 @@ def augment_sigma_k(mt, sigma, k):
     possible = sigma_1k(sigma, k)
     for word in possible:
         if word not in mt.tableau["NEW-WORD"]:
-            mt.tableau["NEW-WORD"][word] = [0.0, None, 0]
+            mt.tableau["NEW-WORD"][word] = [0.0, {}, 0]
 
 def violations(constraint,word):
     return len(constraint.findall(word))#, overlapped = True))
@@ -80,14 +80,14 @@ def apply_mark_list(mt, markList):
     'Apply markedness constraints to each SR in tableau'
     #only add constraint violations if you haven't already added these constraints to the tableau
     if all([con not in mt.constraints for con in markList]):
-        for UR in mt.tableau:
-            for SR in mt.tableau[UR]:
-                if not mt.tableau[UR][SR][1]:
-                    mt.tableau[UR][SR][1] = [violations(re.compile(con),SR) for con in markList]
-                else:
-                    mt.tableau[UR][SR][1].extend([violations(re.compile(con),SR) for con in markList])
+        new_cons_start = len(mt.constraints) # index of the first newly added constraint
         mt.constraints.extend(markList)
         mt.constraints_abbrev.extend(markList)
+        for UR in mt.tableau:
+            for SR in mt.tableau[UR]:
+                for c in range(new_cons_start,len(mt.constraints)):
+                    mt.tableau[UR][SR][1][c] = violations(re.compile(mt.constraints[c]),SR)
+
 	#weights already added by read_constraints()
 	#fwiw, mt.weights has been converted to a numpy array
 	#such things do not support extension or appending
