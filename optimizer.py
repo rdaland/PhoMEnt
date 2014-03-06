a242ed808eb1152bffd26079f1fe63b3372f95a3
bhallen <allenblakeh@gmail.com>
2014-03-05 17:41:30 -0800

    Fix the L1/L2 priors
    I took the example formulas that Robert had added in in-line comments and used them for calculating the actual gradients, fiddling with signs until they seemed intuitively reasonable and the learned weights looked good.  I also made the prior variable names more consistent, and set the bounds to (-50, 0) instead of (-25, 0), as suggested in class.

--- optimizer.py
+++ optimizer.py
@@ -35,7 +35,7 @@ def update_maxent_values(weights, tableau):
 
 ### OBJECTIVE FUNCTION(S) ###
 
-def neg_log_probability_with_gradient(weights, tableau, l1_prior=1.0, l2_prior=0.0):
+def neg_log_probability_with_gradient(weights, tableau, l1_mult=1.0, l2_mult=0.0):
     """ Returns the negative log probability of the data AND a gradient vector.
     This is the objective function used in learn_weights().
     """
@@ -45,8 +45,12 @@ def neg_log_probability_with_gradient(weights, tableau, l1_prior=1.0, l2_prior=0
     expected = [0 for i in range(len(weights))] # Vector of expected violations
     data_size = 0 # Number of total data points: the sum of all counts.
 
-    prob_prior = sum([(l1_prior*w) + (l2_prior*(w**2)) for w in weights])
-    grad_prior = [(l1_prior) + (2*l2_prior) for w in weights]
+    l1_prob_prior = l1_mult * sum(weights)
+    l2_prob_prior = l2_mult * sum(weights*weights)
+    l1_grad_prior = l1_mult * scipy.ones(len(weights))
+    l2_grad_prior = 2 * l2_mult * weights
+    prob_prior = -(l1_prob_prior + l2_prob_prior)
+    grad_prior = -(l1_grad_prior + l2_grad_prior)
 
     for ur in tableau:
         z = z_score(tableau, ur)
@@ -66,7 +70,7 @@ def neg_log_probability_with_gradient(weights, tableau, l1_prior=1.0, l2_prior=0
 
 nlpwg = neg_log_probability_with_gradient # So you don't get carpal tunnel syndrome.
 
-def neg_log_probability(weights, tableau, l1_prior=1.0, l2_prior=0.0):
+def neg_log_probability(weights, tableau, l1_mult=1.0, l2_mult=0.0):
     """ Returns just the negative log probability of the data.
     This function isn't currently used, it's just here in case you want it.
     """
@@ -81,22 +85,17 @@ def probability(weights, tableau):
 
 ### OPTIMIZATION FUNCTION
 
-def learn_weights(mt, L1 = 1.0, L2 = 0.0, precision = 10000000):
+def learn_weights(mt, l1_mult = 1.0, l2_mult = 0.0, precision = 10000000):
     """ Given a filled-in megatableau, return the optimal weight vector.
     """
     # Set up the initial weights and weight bounds (nonpositive reals)
     w_0 = -scipy.rand(len(mt.weights))
-    nonpos_reals = [(-25,0) for wt in mt.weights]
+    nonpos_reals = [(-50,0) for wt in mt.weights]
 
-    # optimization parameters
-    l1_reg = L1 or 1.0        # TODO: logPrior = -l1_reg * sum(weights)
-                              # TODO: logPriorGradient = l1_reg * scipy.ones(len(weights))
-    l2_reg = L2 or 0.0        # TODO: logPrior -= l2_reg * sum(weights*weights)
-                              # TODO: logPriorGradient -= 2*l2_reg * scipy.array(weights)
     prec = precision or 10000000 # TODO: plus prec into optimize call
 
     # Find the best weights
-    learned_weights, fneval, rc = scipy.optimize.fmin_l_bfgs_b(nlpwg, w_0, args = (mt.tableau,l1_reg,l2_reg), bounds=nonpos_reals)
+    learned_weights, fneval, rc = scipy.optimize.fmin_l_bfgs_b(nlpwg, w_0, args = (mt.tableau,l1_mult,l2_mult), bounds=nonpos_reals)
 
     # Update the mt in place with the new weights
     mt.weights = learned_weights
