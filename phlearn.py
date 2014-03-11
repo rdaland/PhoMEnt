f6d859be6ed4ab3a4ac2a1065cb5bc1f14d2255c
michaellefkowitz <lmlefkowitz@gmail.com>
2014-03-11 12:14:29 -0700

    Added --timed to phlearn.py, deleted phlearn2.py
    Use the flag -t or --timed to print out how many seconds each part of
    the phlearning algorithm takes.

--- phlearn.py
+++ phlearn.py
@@ -1,8 +1,14 @@
 import argparse
-
 import megatableau
 import geneval
 import optimizer
+import time
+
+'''phlearn2.py is just like phlearn, but prints out how long various steps take.
+'''
+
+    ### TIME
+mark = time.time()
 
 #####################################################################
 ## Parse command line arguments
@@ -13,6 +19,7 @@ parser.add_argument('constraint_file_name', help='Name of constraints file')
 parser.add_argument('-a', '--alphabet_file_name', default=None, help='List of segments in alphabet; one per line')
 parser.add_argument('-m', '--maxstrlen', type=int, default=5, help='Maximum string length in contrast set')
 parser.add_argument('-o', '--outfile', help='Name of output file')
+parser.add_argument('-t', '--timed', help='Print how much time various parts of the algorithm take.', action="store_true")
 
 ## weight-setting parameters
 parser.add_argument('-l', '--L1', type=float, default=0.0, help='Multiplier for L1 regularizer')
@@ -20,6 +27,14 @@ parser.add_argument('-L', '--L2', type=float, default=0.0, help='Multiplier for 
 parser.add_argument('-p', '--precision', type=float, default=10000000, help='Precision for gradient search (see docs)')
 
 args = parser.parse_args()
+
+### TIMER - keep track of how long each step takes
+mark = time.time()
+
+def timer(task):
+    print task, "\t", round(time.time() - mark, 4), "sec"
+    return time.time()
+
 
 #####################################################################
 ## Main code
@@ -29,7 +44,10 @@ args = parser.parse_args()
 mt = megatableau.MegaTableau()
 
 # read in attested forms and add to MegaTableau
-geneval.read_data_only(mt, args.attested_file_name) 
+geneval.read_data_only(mt, args.attested_file_name)
+
+if args.timed:
+    mark = timer("\nParsed arguments:")
 
 # get alphabet
 if args.alphabet_file_name:
@@ -37,17 +55,32 @@ if args.alphabet_file_name:
 else:
     alphabet = geneval.read_sigma(mt)
 
+if args.timed:
+    mark = timer("Inferred alphabet:")
+
 ## read in constraints
 constraints = geneval.read_constraints(mt, args.constraint_file_name)
 
+if args.timed:
+    mark = timer("Read constraints:")
+
 ## add non-attested forms to tableau
-geneval.augment_sigma_k(mt, alphabet, args.maxstrlen) 
+geneval.augment_sigma_k(mt, alphabet, args.maxstrlen)
+
+if args.timed:
+    mark = timer("Augmented with GEN:")
 
 ## apply constraints (this is the costliest step...)
 geneval.apply_mark_list(mt, constraints)
 
+if args.timed:
+    mark = timer("Computed violations:")
+
 # Optimize mt.weights
 optimizer.learn_weights(mt, args.L1, args.L2, args.precision)
+
+if args.timed:
+    mark = timer("Optimized weights:")
 
 # Write the final MegaTableau to file
 if args.outfile:
